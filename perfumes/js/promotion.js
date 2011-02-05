e6a32dfbacf076fa6421fcec8f02ac2c4210e40b
rbrito1978 <rbrito1978@06bfaf6f-17ae-4573-239b-02af22a1a53f>
2011-02-05 11:28:30 +0000

    Js para amostras.html
    git-svn-id: http://incolume.googlecode.com/svn/trunk@217 06bfaf6f-17ae-4573-239b-02af22a1a53f

--- perfumes/js/promotion.js
+++ perfumes/js/promotion.js
@@ -27,3 +27,63 @@ function selecionaItem(coditem, elem_valor, desc, vol){
 function showmessage(msg){
   alert(msg);
 }
+function CheckAll(chk){
+  for (i = 0; i < chk.length; i++)
+    chk[i].checked = true ;
+  checkItem();
+}
+function UnCheckAll(chk){
+  for (i = 0; i < chk.length; i++)
+    chk[i].checked = false ;
+  checkItem();
+}
+function freteOff(obj,valor){
+  document.getElementById(obj).value = valor;
+}
+function checkItem(){
+	check = document.form.item_id;
+        freteOff('extras','');
+	cod_item = "";
+	compra = "";
+	count = "";
+	if(check.length){
+		for (i = 0; i < check.length; i++){
+		    if(check[i].checked){
+		        cod_item = check[i].value;
+			nome_campo = "item_id_" + cod_item;
+			count++;
+			valor='';
+			if (count <= 1){
+			  valor='1000';
+			}else if (count > 1 && count <=2){
+			  valor='850';
+			}else if (count > 2 && count <=3){
+			  valor='750';
+			}else if (count > 3 && count <=4){
+			  valor='700';
+			}else if (count > 4 && count <=5){
+			  valor='550';
+			}else {
+			  valor='500';
+			}
+			   //document.getElementById(nome_campo).value = cod_item;	/* fazer conforme o outro script usado com o select */
+			   prod = '<input type="" size="" name="item_id_'+  count  +'" id="item_id_' +  count  +'" value="'+ cod_item +'" />'
+  			   prod = prod + '<input type="" size="" name="item_descr_'+ count +'" id="item_descr_'+ count +'" value="Ecssus '+ cod_item+' 5ml" />'
+			   prod = prod + '<input type="" size="" name="item_quant_'+ count +'" id="item_quant_'+ count +'" value="1" />'
+			   prod = prod + '<input type="" size="" name="item_valor_'+ count +'" id="item_valor_'+ count +'" value="'+valor+'" />'
+			   prod = prod + '<input type="" size="" name="item_frete_'+ count +'" id="item_frete_'+ count +'" value="1000" />'
+			   prod = prod + '<input type="" size="" name="item_peso_'+ count +'" id=" item_peso_'+ count +'" value="0" /> <br />'
+			   compra = compra + prod;
+		    }
+		}
+		if (count >= 3){
+		   c = (count -1)*5+10
+		   freteOff('extras',c*-100)
+		}
+	}else{
+		if(check.checked)
+		    cod_item = document.form.item_id.value; /* fazer conforme o outro script usado com o select */
+	}
+	//document.write(prod);
+	document.getElementById('produtos').innerHTML = compra + count;
+}

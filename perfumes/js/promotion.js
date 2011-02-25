e5ac39d1e74d52d42972e57ce2030388a2fd0392
rbrito1978 <rbrito1978@06bfaf6f-17ae-4573-239b-02af22a1a53f>
2011-02-25 19:30:11 +0000

    Retorno ao js inicial, não é necessário alteração no js para contemplar os porta perfumes
    git-svn-id: http://incolume.googlecode.com/svn/trunk@259 06bfaf6f-17ae-4573-239b-02af22a1a53f

--- perfumes/js/promotion.js
+++ perfumes/js/promotion.js
@@ -24,12 +24,6 @@ function selecionaItem(coditem, elem_valor, desc, vol){
   document.getElementById(cod_item).value = elem_valor;
   document.getElementById(desc_item).value = desc + elem_valor + vol;
 }
-function selecionaRadio(coditem, elem_valor, desc){
-  var cod_item = "item_id_" + coditem;
-  var desc_item = "item_descr_" + coditem;
-  document.getElementById(cod_item).value = elem_valor;
-  document.getElementById(desc_item).value = desc + elem_valor;
-}
 function showmessage(msg){
   alert(msg);
 }

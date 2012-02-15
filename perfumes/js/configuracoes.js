5f89e7eab2a42a1c234abc58b4266026c31b2b0b
rbrito1978@gmail.com <rbrito1978@gmail.com@06bfaf6f-17ae-4573-239b-02af22a1a53f>
2012-02-15 15:19:10 +0000

    atualização frete 17
    git-svn-id: http://incolume.googlecode.com/svn/trunk@379 06bfaf6f-17ae-4573-239b-02af22a1a53f

--- perfumes/js/configuracoes.js
+++ perfumes/js/configuracoes.js
@@ -10,7 +10,7 @@ configuracoes = {
                       // Aqui você deverá informar EN para PAC ou SD para Sedex
                       // Não se esqueça de definir suas opções no seu Admin do PagSeguro
 //  frete       : 15,
-  peso        : 9,
+  peso        : 17,
 
   // Abaixo, coloque as referencias aos seus elementos do HTML
   id_menu     : 'vitrinemenu',

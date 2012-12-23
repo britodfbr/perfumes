915cdb495247c07ac83780f7af8488acadd230ff
rbrito1978@gmail.com <rbrito1978@gmail.com@06bfaf6f-17ae-4573-239b-02af22a1a53f>
2012-12-23 11:22:47 +0000

    paypal, botão
    git-svn-id: http://incolume.googlecode.com/svn/trunk@394 06bfaf6f-17ae-4573-239b-02af22a1a53f

--- perfumes/js/pagseguro.js
+++ perfumes/js/pagseguro.js
@@ -113,6 +113,7 @@ PagSeguro.prototype.atualizaCarrinho = function() {
   tabela+='</tbody><tfoot><tr><th colspan="3">Total</th><th colspan="2">'+pgs.moeda(total)+'</th></tr></tfoot></table>'
   ret += '<input type=button onclick="window.location.reload( true );" value="Limpar Todos" alt="Limpar todos os itens da compra! :( " >'
   ret += '<input type=button onclick="parentNode.submit()" value="Finalizar Compra PagSeguro" alt="Finalizar compra! " >\n'
+  ret += '<input type=button onclick="parentNode.submit()" value="Finalizar Compra PayPal" alt="Finalizar compra! " >\n'
   /*ret += '<input type="image" src="'+this.configuracoes.imgBotao+'" name="submit" alt="Finalizar compra!" />\n'*/
   ret += '</form> Sua compra garantida ou seu dinheiro de volta! <h2> </h2>'
   document.getElementById(this.configuracoes.id_carrinho).innerHTML = tabela+ret

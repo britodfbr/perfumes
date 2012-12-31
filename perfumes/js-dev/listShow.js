750e9427f3b3a5681bcb785e5b7637b21ddf655d
rbrito1978@gmail.com <rbrito1978@gmail.com@06bfaf6f-17ae-4573-239b-02af22a1a53f>
2012-12-31 10:13:12 +0000

    include conteudo externo
    git-svn-id: http://incolume.googlecode.com/svn/trunk@431 06bfaf6f-17ae-4573-239b-02af22a1a53f

---
+++ perfumes/js-dev/listShow.js
@@ -0,0 +1,47 @@
+ecssusListShow(){
+document.write("Copyright Jennifer Kyrnin 1992");
+//Ecssus #01 – feminino  – Amor Amor<br /> 
+//Ecssus #02 – masculino – Azarro Chrome<br /> 
+//Ecssus #03 – feminino  – Anais Anais<br /> 
+//Ecssus #04 – unissex   – Bvlgari Black<br /> 
+//Ecssus #05 – feminino  – Angel<br /> 
+//Ecssus #06 – unissex   – C.K. One<br /> 
+//Ecssus #07 – feminino  – Carolina Herreira<br /> 
+//Ecssus #08 – masculino – Dolce &amp; Gabana<br /> 
+//Ecssus #09 – feminino  – Chance<br /> 
+//Ecssus #10 – masculino – Eternity<br /> 
+//Ecssus #11 – feminino  – Channel n&#0186;5<br /> 
+//Ecssus #12 – masculino – Ferrari Black<br /> 
+//Ecssus #13 – feminino  – Dolce &amp; Gabana<br /> 
+//Ecssus #14 – masculino – Ferrari Red<br /> 
+//Ecssus #15 – feminino  – Eternity<br /> 
+//Ecssus #16 – masculino – Hugo Boss<br /> 
+//Ecssus #17 – feminino  – Flower<br /> 
+//Ecssus #18 – masculino – Joop Night Flight<br /> 
+//Ecssus #19 – feminino  – Gabriela Sabatini<br /> 
+//Ecssus #20 – masculino – Kouros<br /> 
+//Ecssus #21 – feminino  – Hipnose<br /> 
+//Ecssus #22 – masculino – Kouros Fraugher<br /> 
+//Ecssus #23 – feminino  – J&#0146;Adore<br /> 
+//Ecssus #24 – masculino – le Malle<br /> 
+//Ecssus #25 – feminino  – Laguna<br /> 
+//Ecssus #26 – masculino – L&#0146;Eu D&#0146;Issy Homme<br /> 
+//Ecssus #27 – feminino  – Mont Blac Presence<br /> 
+//Ecssus #28 – masculino – Minotaure<br /> 
+//Ecssus #29 – unissex   – Nat Magic Baby (infantil)<br /> 
+//Ecssus #30 – masculino – Mont Blac Presence<br /> 
+//Ecssus #31 – feminino  – Paris<br /> 
+//Ecssus #32 – masculino – Pi Givanchi<br /> 
+//Ecssus #33 – feminino  – Polo Sport<br /> 
+//Ecssus #34 – masculino – Polo Blue<br /> 
+//Ecssus #35 – feminino  – Ralf For Womam<br /> 
+//Ecssus #36 – masculino – Polo Sport<br /> 
+//Ecssus #37 – feminino  – Sense<br /> 
+//Ecssus #38 – masculino – Polo Verde<br /> 
+//Ecssus #39 – feminino  – 212 Sexy<br /> 
+//Ecssus #40 – masculino – Strong Man<br />
+//Ecssus #41 – Feminino – Fantasy<br />
+//Ecssus #42 – Masculino – One Million<br />
+//Ecssus #43 – Feminino – Tommy Girl<br />
+//Ecssus #44 – Unissex – Calvin Klein Be<br />
+}

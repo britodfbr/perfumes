098941d519b2b041a859051704eb900c304c21ed
rbrito1978@gmail.com <rbrito1978@gmail.com@06bfaf6f-17ae-4573-239b-02af22a1a53f>
2012-12-31 10:32:10 +0000

    include conteudo externo via js
    git-svn-id: http://incolume.googlecode.com/svn/trunk@432 06bfaf6f-17ae-4573-239b-02af22a1a53f

--- perfumes/js-dev/listShow.js
+++ perfumes/js-dev/listShow.js
@@ -1,47 +1,47 @@
-ecssusListShow(){
-document.write("Copyright Jennifer Kyrnin 1992");
-//Ecssus #01 – feminino  – Amor Amor<br /> 
-//Ecssus #02 – masculino – Azarro Chrome<br /> 
-//Ecssus #03 – feminino  – Anais Anais<br /> 
-//Ecssus #04 – unissex   – Bvlgari Black<br /> 
-//Ecssus #05 – feminino  – Angel<br /> 
-//Ecssus #06 – unissex   – C.K. One<br /> 
-//Ecssus #07 – feminino  – Carolina Herreira<br /> 
-//Ecssus #08 – masculino – Dolce &amp; Gabana<br /> 
-//Ecssus #09 – feminino  – Chance<br /> 
-//Ecssus #10 – masculino – Eternity<br /> 
-//Ecssus #11 – feminino  – Channel n&#0186;5<br /> 
-//Ecssus #12 – masculino – Ferrari Black<br /> 
-//Ecssus #13 – feminino  – Dolce &amp; Gabana<br /> 
-//Ecssus #14 – masculino – Ferrari Red<br /> 
-//Ecssus #15 – feminino  – Eternity<br /> 
-//Ecssus #16 – masculino – Hugo Boss<br /> 
-//Ecssus #17 – feminino  – Flower<br /> 
-//Ecssus #18 – masculino – Joop Night Flight<br /> 
-//Ecssus #19 – feminino  – Gabriela Sabatini<br /> 
-//Ecssus #20 – masculino – Kouros<br /> 
-//Ecssus #21 – feminino  – Hipnose<br /> 
-//Ecssus #22 – masculino – Kouros Fraugher<br /> 
-//Ecssus #23 – feminino  – J&#0146;Adore<br /> 
-//Ecssus #24 – masculino – le Malle<br /> 
-//Ecssus #25 – feminino  – Laguna<br /> 
-//Ecssus #26 – masculino – L&#0146;Eu D&#0146;Issy Homme<br /> 
-//Ecssus #27 – feminino  – Mont Blac Presence<br /> 
-//Ecssus #28 – masculino – Minotaure<br /> 
-//Ecssus #29 – unissex   – Nat Magic Baby (infantil)<br /> 
-//Ecssus #30 – masculino – Mont Blac Presence<br /> 
-//Ecssus #31 – feminino  – Paris<br /> 
-//Ecssus #32 – masculino – Pi Givanchi<br /> 
-//Ecssus #33 – feminino  – Polo Sport<br /> 
-//Ecssus #34 – masculino – Polo Blue<br /> 
-//Ecssus #35 – feminino  – Ralf For Womam<br /> 
-//Ecssus #36 – masculino – Polo Sport<br /> 
-//Ecssus #37 – feminino  – Sense<br /> 
-//Ecssus #38 – masculino – Polo Verde<br /> 
-//Ecssus #39 – feminino  – 212 Sexy<br /> 
-//Ecssus #40 – masculino – Strong Man<br />
-//Ecssus #41 – Feminino – Fantasy<br />
-//Ecssus #42 – Masculino – One Million<br />
-//Ecssus #43 – Feminino – Tommy Girl<br />
-//Ecssus #44 – Unissex – Calvin Klein Be<br />
+function ecssusListShow(){
+/*document.write("Copyright Jennifer Kyrnin 1992");*/
+document.write("Ecssus #01 &#8212; feminino &#8212;  Amor Amor<br />"); 
+document.write("Ecssus #02 &#8212; masculino &#8212; Azarro Chrome<br />"); 
+document.write("Ecssus #03 &#8212; feminino &#8212;  Anais Anais<br />"); 
+document.write("Ecssus #04 &#8212; unissex &#8212;   Bvlgari Black<br />"); 
+document.write("Ecssus #05 &#8212; feminino &#8212;  Angel<br />"); 
+document.write("Ecssus #06 &#8212; unissex &#8212;   C.K. One<br />"); 
+document.write("Ecssus #07 &#8212; feminino &#8212;  Carolina Herreira<br />"); 
+document.write("Ecssus #08 &#8212; masculino &#8212; Dolce &amp; Gabana<br />"); 
+document.write("Ecssus #09 &#8212; feminino &#8212;  Chance<br />"); 
+document.write("Ecssus #10 &#8212; masculino &#8212; Eternity<br />"); 
+document.write("Ecssus #11 &#8212; feminino &#8212;  Channel n&#0186;5<br />"); 
+document.write("Ecssus #12 &#8212; masculino &#8212; Ferrari Black<br />"); 
+document.write("Ecssus #13 &#8212; feminino &#8212;  Dolce &amp; Gabana<br />"); 
+document.write("Ecssus #14 &#8212; masculino &#8212; Ferrari Red<br />"); 
+document.write("Ecssus #15 &#8212; feminino &#8212;  Eternity<br />"); 
+document.write("Ecssus #16 &#8212; masculino &#8212; Hugo Boss<br />"); 
+document.write("Ecssus #17 &#8212; feminino &#8212;  Flower<br />"); 
+document.write("Ecssus #18 &#8212; masculino &#8212; Joop Night Flight<br />"); 
+document.write("Ecssus #19 &#8212; feminino &#8212;  Gabriela Sabatini<br />"); 
+document.write("Ecssus #20 &#8212; masculino &#8212; Kouros<br />"); 
+document.write("Ecssus #21 &#8212; feminino &#8212;  Hipnose<br />"); 
+document.write("Ecssus #22 &#8212; masculino &#8212; Kouros Fraugher<br />"); 
+document.write("Ecssus #23 &#8212; feminino &#8212;  J&#0146;Adore<br />"); 
+document.write("Ecssus #24 &#8212; masculino &#8212; le Malle<br />"); 
+document.write("Ecssus #25 &#8212; feminino &#8212;  Laguna<br />"); 
+document.write("Ecssus #26 &#8212; masculino &#8212; L&#0146;Eu D&#0146;Issy Homme<br />"); 
+document.write("Ecssus #27 &#8212; feminino &#8212;  Mont Blac Presence<br />"); 
+document.write("Ecssus #28 &#8212; masculino &#8212; Minotaure<br />"); 
+document.write("Ecssus #29 &#8212; unissex &#8212;   Nat Magic Baby (infantil)<br />"); 
+document.write("Ecssus #30 &#8212; masculino &#8212; Mont Blac Presence<br />"); 
+document.write("Ecssus #31 &#8212; feminino &#8212;  Paris<br />"); 
+document.write("Ecssus #32 &#8212; masculino &#8212; Pi Givanchi<br />"); 
+document.write("Ecssus #33 &#8212; feminino &#8212;  Polo Sport<br />"); 
+document.write("Ecssus #34 &#8212; masculino &#8212; Polo Blue<br />"); 
+document.write("Ecssus #35 &#8212; feminino &#8212;  Ralf For Womam<br />"); 
+document.write("Ecssus #36 &#8212; masculino &#8212; Polo Sport<br />"); 
+document.write("Ecssus #37 &#8212; feminino &#8212;  Sense<br />"); 
+document.write("Ecssus #38 &#8212; masculino &#8212; Polo Verde<br />"); 
+document.write("Ecssus #39 &#8212; feminino &#8212;  212 Sexy<br />"); 
+document.write("Ecssus #40 &#8212; masculino &#8212; Strong Man<br />");
+document.write("Ecssus #41 &#8212; Feminino &#8212; Fantasy<br />");
+document.write("Ecssus #42 &#8212; Masculino &#8212; One Million<br />");
+document.write("Ecssus #43 &#8212; Feminino &#8212; Tommy Girl<br />");
+document.write("Ecssus #44 &#8212; Unissex &#8212; Calvin Klein Be<br />");
 }

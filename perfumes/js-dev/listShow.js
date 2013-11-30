e14d4b669311577c493ed772464f6c248f9ec055
Brito <britodfbr@gmail.com>
2013-11-30 18:40:10 -0200

    apresentação corrigida

--- perfumes/js-dev/listShow.js
+++ perfumes/js-dev/listShow.js
@@ -1,3 +1,51 @@
+function ecssusListShow2013(){
+/*document.write("(C) Incolume.com 2010");*/
+document.write("Ecssus #01 &#8212; Feminino &#8212; Amor Amor  <br />");
+document.write("Ecssus #02 &#8212; Masculino &#8212; Azarro Chrome  <br />");
+document.write("Ecssus #03 &#8212; Feminino &#8212; Anais Anais  <br />");
+document.write("Ecssus #04 &#8212; Unissex &#8212; Bvlgari Black  <br />");
+document.write("Ecssus #05 &#8212; Feminino &#8212; Angel   <br />");
+document.write("Ecssus #06 &#8212; Unissex &#8212; Calvin Klein One <br />");
+document.write("Ecssus #07 &#8212; Feminino &#8212; Carolina Herreira  <br />");
+document.write("Ecssus #08 &#8212; Masculino &#8212; Dolce &amp; Gabana <br />");
+document.write("Ecssus #09 &#8212; Feminino &#8212; Chance   <br />");
+document.write("Ecssus #10 &#8212; Masculino &#8212; 212 VIP  <br />");
+document.write("Ecssus #11 &#8212; Feminino &#8212; Channel n&#0186;5  <br />");
+document.write("Ecssus #12 &#8212; Masculino &#8212; Ferrari Black  <br />");
+document.write("Ecssus #13 &#8212; Feminino &#8212; Dolce &amp; Gabana <br />");
+document.write("Ecssus #14 &#8212; Masculino &#8212; Ferrari Red  <br />");
+document.write("Ecssus #15 &#8212; Feminino &#8212; Eternity   <br />");
+document.write("Ecssus #16 &#8212; Masculino &#8212; Hugo Boss  <br />");
+document.write("Ecssus #17 &#8212; Feminino &#8212; Flower   <br />");
+document.write("Ecssus #18 &#8212; Masculino &#8212; Joop Night Flight <br />");
+document.write("Ecssus #19 &#8212; Feminino &#8212; Gabriela Sabatini  <br />");
+document.write("Ecssus #20 &#8212; Masculino &#8212; Kouros   <br />");
+document.write("Ecssus #21 &#8212; Feminino &#8212; Hipnose   <br />");
+document.write("Ecssus #22 &#8212; Masculino &#8212; Kouros Fraugher  <br />");
+document.write("Ecssus #23 &#8212; Feminino &#8212; J&#0146;Adore   <br />");
+document.write("Ecssus #24 &#8212; Masculino &#8212; Le Malle  <br />");
+document.write("Ecssus #25 &#8212; Feminino &#8212; Laguna   <br />");
+document.write("Ecssus #26 &#8212; Masculino &#8212; XS Black  <br />");
+document.write("Ecssus #27 &#8212; Feminino &#8212; XS Black  <br />");
+document.write("Ecssus #28 &#8212; Masculino &#8212; Minotaure   <br />");
+document.write("Ecssus #29 &#8212; Feminino &#8212; Dolce&amp;Gabana Light Blue <br />");
+document.write("Ecssus #30 &#8212; Masculino &#8212; Mont Blac Presence <br />");
+document.write("Ecssus #31 &#8212; Feminino &#8212; Paris   <br />");
+document.write("Ecssus #32 &#8212; Masculino &#8212; Pi Givanchi  <br />");
+document.write("Ecssus #33 &#8212; Feminino &#8212; Polo Sport  <br />");
+document.write("Ecssus #34 &#8212; Masculino &#8212; Polo Blue  <br />");
+document.write("Ecssus #35 &#8212; Feminino &#8212; 212 VIP  <br />");
+document.write("Ecssus #36 &#8212; Masculino &#8212; Polo Sport  <br />");
+document.write("Ecssus #37 &#8212; Feminino &#8212; Sense   <br />");
+document.write("Ecssus #38 &#8212; Masculino &#8212; Polo Verde  <br />");
+document.write("Ecssus #39 &#8212; Feminino &#8212; 212 Sexy  <br />");
+document.write("Ecssus #40 &#8212; Masculino &#8212; Strong Man  <br />");
+document.write("Ecssus #41 &#8212; Feminino &#8212; Fantasy   <br />");
+document.write("Ecssus #42 &#8212; Masculino &#8212; One Million  <br />");
+document.write("Ecssus #43 &#8212; Feminino &#8212; Tommy Girl  <br />");
+document.write("Ecssus #44 &#8212; Unissex &#8212; Calvin Klein Be <br />");
+}
+
 function ecssusListShow(){
 /*document.write("(C) Incolume.com 2010");*/
 document.write("Ecssus #01 &#8212; feminino &#8212;  Amor Amor<br />"); 
